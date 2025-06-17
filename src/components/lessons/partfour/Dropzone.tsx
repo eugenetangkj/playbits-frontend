f50d1a4c938d8c2cce3498272b3edfd2f1bd8a93
eugenetangkj <dragoneugene10@gmail.com>
2025-06-18 00:49:00 +0800

    enhancement: Adjust dropzone size

--- src/components/lessons/partfour/Dropzone.tsx
+++ src/components/lessons/partfour/Dropzone.tsx
@@ -14,7 +14,7 @@ export const DropZone = ({ id, value }: DropZoneProps) => {
   return (
     <span
       ref={setNodeRef}
-      className={`inline-block min-w-[100px] min-h-[100px] px-2 py-1 border-2 rounded-md transition-colors duration-200 ${
+      className={`inline-block min-w-[200px] min-h-[60px] md:min-h-[60px] px-2 py-1 border-2 rounded-md transition-colors duration-200 ${
         isOver ? 'border-playbits-blue-900 bg-blue-100' : 'border-dashed border-gray-400'
       } text-center align-middle`}
     >
